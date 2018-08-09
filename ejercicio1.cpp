88bd5a2e81a191140ba3b7687d74e1060ef0338b
hugoalbeirom <42081531+hugoalbeirom@users.noreply.github.com>
2018-08-08 21:36:35 -0500

    ejercicios de clase
    sin terminar

---
+++ ejercicio1.cpp
@@ -0,0 +1,35 @@
+// Example program
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string name="John";
+  std::string surname="Doe";
+  std::string fullname;
+  fullname = name + " " + surname;
+  std::cout << "your name is: " << fullname << std::endl;
+  return 0;
+}
+
+
+// Example program
+#include <iostream>
+#include <string>
+
+int main()
+{
+int #1, #2;
+#1=10
+#2=20
+
+#1<#2
+#1>#2
+#1=#2
+
+std::cin <<ingrese #1>>
+std::cin <<ingrese #2>>
+
+condition? result1:result2;
+  //return 0;
+}
